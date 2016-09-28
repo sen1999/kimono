60a75f88cc2e223d5719f979464b802a40394214
TAK-PC\konni <renshin.aka@gmail.com>
2016-09-28 17:53:41 +0900

    new: copy javascript from bolt templete

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,4 @@
+class ListController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'coodinate#index'
   resources :tansu
+  resources :list
 end
