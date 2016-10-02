3c457ba1afd822ab7cb7812bc1afba5514130a28
TAK-PC\konni <renshin.aka@gmail.com>
2016-10-02 20:35:40 +0900

    delete; route -only

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'sets#index'
   resources :lists
-  resources :items, only: [:new, :create, :destroy, :edit, :update]
+  resources :items
 end
