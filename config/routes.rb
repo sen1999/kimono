cad108adc998e01975e08519c4e9ca81e20a54b4
TAK-PC\konni <renshin.aka@gmail.com>
2016-10-02 11:40:31 +0900

    setsとlists controller 作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'coodinate#index'
-  resources :list
+  resources :lists
   resources :tansu
 end
---
+++ test/controllers/sets_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SetsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
