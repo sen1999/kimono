df1c4242cfa3d1cb498c6b428b16b60ad8b2c185
TAK-PC\konni <renshin.aka@gmail.com>
2016-10-06 21:38:38 +0900

    error; params がうまくいかぬ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,25 +2,44 @@ class ItemsController < ApplicationController
 
   def index
     @kimonos = Kimono.all
+    @belts = Belt.all
+    @collars = Collar.all
   end
 
   def show
     @kimono = Kimono.find(params[:id])
+    # @belt = Belt.find(params[:id])
   end
 
   def new
     @kimonos = Kimono.all
     @kimono = Kimono.new
+
+    @belts = Belt.all
+    @belt = Belt.new
+
+    @collars = Collar.all
+    @collar = Collar.new
   end
 
   def create
-    Kimono.create(create_params)
+    Kimono.create(kimono_params)
     @kimonos = Kimono.all
+
+    # Collar.create(collar_params)
+    # @collars = Collar.all
+
+
+    # Belt.create(belt_params)
+    # @belt = Belt.all
+    redirect_to :acton => "items#index"
   end
 
   def destroy
     Kimono.find(params[:id]).destroy
     @kimonos = Kimono.all
+    # Belt.find(params[:id]).destroy
+    # @belts = Belt.all
   end
 
   def edit
@@ -30,13 +49,21 @@ class ItemsController < ApplicationController
   def update
     # @kimono = Kimono.find(params[:id]).update(update_params)
     Kimono.update(update_params)
+    redirect_to :bacj
   end
 private
-  def create_params
+
+
+  def kimono_params
     params.require(:kimono).permit(:image, :use_list)
   end
 
-  def update_params
-    params.require(:kimono).permit(:image, :use_list)
-  end
+
+  # def belt_params
+  #   params.require(:belt).permit(:image, :use_list)
+  # end
+
+  #   def collar_params
+  #   params.require(:collar).permit(:image, :use_list)
+  # end
 end
--- app/models/belt.rb
+++ app/models/belt.rb
@@ -1,2 +1,6 @@
 class Belt < ActiveRecord::Base
+    has_attached_file :image, styles: { medium: "100x100 >", thumb: "100x100>"}
+  validates_attachment_content_type :image, content_type: ["image/jpg","image/jpeg","image/png","image/gif"]
+
+  acts_as_ordered_taggable_on :uses
 end
