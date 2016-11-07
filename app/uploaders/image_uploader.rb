5358cc52278880e09d878954fb3f31e2aae0e4dc
TAK-PC\konni <renshin.aka@gmail.com>
2016-11-08 02:42:45 +0900

    new; (:thumb)のリサイズはできた next; トリミング

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,10 +31,37 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    "#{Time.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename.present?
+  end
+
+
+
   # Create different versions of your uploaded files:
   version :thumb do
+    # process :crop
     process :resize_to_limit => [100, 100]
   end
+
+  # def crop
+  #   manipulate! do |img|
+  #     gravity = Magick::CenterGravity # 中央から切り取ります。
+  #     crop_w = img.columns
+  #     crop_h = img.rows
+  #     # 画像のサイズが縦横違った場合は小さい方に合わせてトリミングする。
+  #     if img.rows <= img.columns
+  #       crop_w = img.rows
+  #     else
+  #       crop_h = img.columns
+  #     end
+  #     img.crop!(gravity, crop_w, crop_h)
+  #     img = yield(img) if block_given?
+  #     img
+  #   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
