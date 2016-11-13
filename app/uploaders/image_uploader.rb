698e37924b09bca63866f878d060a37ceb52a0dd
TAK-PC\konni <renshin.aka@gmail.com>
2016-11-13 21:42:23 +0900

    ...?

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -43,25 +43,31 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    # process :crop
+    process :crop
     process :resize_to_limit => [100, 100]
   end
 
-  # def crop
-  #   manipulate! do |img|
-  #     gravity = Magick::CenterGravity # 中央から切り取ります。
-  #     crop_w = img.columns
-  #     crop_h = img.rows
-  #     # 画像のサイズが縦横違った場合は小さい方に合わせてトリミングする。
-  #     if img.rows <= img.columns
-  #       crop_w = img.rows
-  #     else
-  #       crop_h = img.columns
-  #     end
-  #     img.crop!(gravity, crop_w, crop_h)
-  #     img = yield(img) if block_given?
-  #     img
-  #   end
+  version :detail do
+    process :crop
+    process :resize_to_limit => [300, 300]
+  end
+
+  def crop
+    manipulate! do |img|
+      gravity = Magick::CenterGravity # 中央から切り取ります。
+      crop_w = img.columns
+      crop_h = img.rows
+      # 画像のサイズが縦横違った場合は小さい方に合わせてトリミングする。
+      if img.rows <= img.columns
+        crop_w = img.rows
+      else
+        crop_h = img.columns
+      end
+      img.crop!(gravity, crop_w, crop_h)
+      img = yield(img) if block_given?
+      img
+    end
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
